b01a61c18ff2719e24dffafe479bfab4850332c1
rekosuo <161256068+rekosuo@users.noreply.github.com>
2026-04-12 02:09:26 +0300

    Removed floating menu button

--- project/app/src/main/java/com/rekosuo/gymtracker/ui/exercise/ExerciseListScreen.kt
+++ project/app/src/main/java/com/rekosuo/gymtracker/ui/exercise/ExerciseListScreen.kt
@@ -1,19 +1,43 @@
 package com.rekosuo.gymtracker.ui.exercise
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.res.painterResource
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.rekosuo.gymtracker.R
 import com.rekosuo.gymtracker.ui.components.ExerciseListItem
 import com.rekosuo.gymtracker.ui.components.GroupListItem
-import com.rekosuo.gymtracker.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -89,11 +113,6 @@ fun ExerciseListScreen(
                     }
                 }
             )
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = onCreateExercise) {
-                Icon(painter = painterResource(id = R.drawable.ic_symbol_add), "Add Exercise")
-            }
         }
     ) { padding ->
         Column(
@@ -226,7 +245,11 @@ fun ExerciseListScreen(
                                     viewModel.onEvent(ExerciseListEvent.ToggleFavorite(exercise))
                                 },
                                 onNavigateToGraph = { onNavigateToGraph(exercise.id) },
-                                onNavigateToExerciseCalendar = { onNavigateToExerciseCalendar(exercise.id) },
+                                onNavigateToExerciseCalendar = {
+                                    onNavigateToExerciseCalendar(
+                                        exercise.id
+                                    )
+                                },
                             )
                         }
                     }
@@ -242,7 +265,7 @@ fun ExerciseListScreen(
                             ) {
                                 Text(
                                     text = if (state.searchQuery.isEmpty()) {
-                                        "No exercises or groups yet.\nTap + to create one!"
+                                        "No exercises or groups yet.\nTap the top menu button to create them!"
                                     } else {
                                         "No results found for \"${state.searchQuery}\""
                                     },
